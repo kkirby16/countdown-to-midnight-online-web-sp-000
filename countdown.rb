1117ea556865c0d4465639c59fc75594ed61591b
Kevin Kirby <kkirby16@my.whitworth.edu>
2020-05-08 16:12:17 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,8 +4,8 @@ def countdown(integer)
   while integer > 0 do 
     puts "#{integer} SECOND(S)!"
     integer -= 1 
-    return "HAPPY NEW YEAR!"
   end
+  return "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep
